13d94589381ae7e0f2c3c6e907230b46b52b50e0
pineapple <m18817830878@163.com>
2018-11-16 18:06:15 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -46,8 +46,8 @@ function drawGradient(){
 	var intervalR = (endR - startR)/steps;//每个圆的半径
 	
 	var gradientcolor=[];
-	var from =color (random(255)/255,random(255)/255,random(255)/255);
-	var to = color(random(255)/255,random(255)/255,random(255)/255);
+	var from =color (123,143,4);
+	var to = color(4,134,221);
 	
 	for(var i=0;i<steps-2;i++){
 		gradientcolor[i] = lerpColor(from, to, i/(steps-2));
